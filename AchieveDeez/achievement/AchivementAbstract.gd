3ea1eeaa6aad4208cc7f2c6123993bcbf426d441
digitalbarrito <j.levandosky@me.com>
2024-10-01 16:30:29 -0700

    Initial commit
    beans

---
+++ AchieveDeez/achievement/AchivementAbstract.gd
@@ -0,0 +1,38 @@
+extends "res://achievement/AchivementAbstract.gd"
+
+
+func validateStatAchievements():
+	CurrentGame.checkGameState()
+	if not CurrentGame.cheetah:
+		for k in achivements:
+			if k.begins_with("stat:"):
+				var stat = k.trim_prefix("stat:")
+				var v = achivements[k]
+				Debug.l("Validating stat %s to %s" % [stat, v])
+				emit_signal("stat", stat, v)
+
+func setStat(stat:String, to)->void :
+	CurrentGame.checkGameState()
+	if not CurrentGame.cheetah:
+		var skey = "stat:%s" % stat
+		var pv = achivements.get(skey, 0)
+		if to > pv:
+			emit_signal("stat", stat, to)
+			achivements[skey] = to
+			Debug.l("Game stat %s raised to %s" % [skey, to])
+			saveToFile()
+		
+
+func achive(what)->void :
+	if not (what in achievementRarity):
+		Debug.l("Illegal achivement %s" % what)
+		return 
+	CurrentGame.checkGameState()
+	if not CurrentGame.cheetah:
+		if not what in achivements:
+			Debug.l("New abstract achivement %s" % what)
+			achivements[what] = true
+			saveToFile()
+			emit_signal("achivedOffline", what)
+			if not CurrentGame.isDemo():
+				emit_signal("achived", what)
